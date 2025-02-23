18b50af782d013dda60d4b718ed99ad88ab7c4fa
Hope141 <nadiaoussatchova@gmail.com>
2025-02-23 20:24:58 +0200

    updated task_2

--- task_2.py
+++ task_2.py
@@ -3,16 +3,15 @@ from pathlib import Path
 def get_cats_info(path):
     try:
         with open (path, 'r', encoding='utf-8') as file:
-                lines = file.readlines()
                 cats_info = []
-                for line in lines:
-                    cats_info.append(line.strip().split(','))
-                if len(cats_info) == 3:  
-                    cat_id, name, age = cats_info
-                    cats_info.append({"id": cat_id, "name": name, "age": age})
-                else:
-                    print(f'ValueError with line: {line}')
-        return cats_info
+                for line in file:
+                    parts = (line.strip().split(','))
+                    if len(parts) == 3:  
+                        cat_id, name, age = parts
+                        cats_info.append({"id": cat_id, "name": name, "age": age})
+                    else:
+                        print(f'ValueError with line: {line}')
+                return cats_info
     except FileNotFoundError:
         print('File not found')
         return None   
